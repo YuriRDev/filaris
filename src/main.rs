f6593bc5f0a6ade77a9a56d8a3042c17e3305137
Yuri <49888082+YuriRDev@users.noreply.github.com>
2024-06-25 11:39:06 -0300

    Removed trash on main

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,4 @@ use scrapper::{analise_page, extract_strings_from_html};
 
 #[tokio::main]
 async fn main() {
-    println!("{}",normalize_url("https://localhost".to_string()));
-}
-
-
-pub fn normalize_url(url: String) -> String {
-    let mut normalized = url.to_string();
-    if url.starts_with("https://") {
-        normalized = normalized.replacen("https://", "", 1);
-    } else if url.starts_with("http://") {
-        normalized = normalized.replacen("http://", "", 1);
-    }
-
-    if normalized.starts_with("www") {
-        normalized = normalized.replacen("www", "", 1);
-    }
-
-    // while (last_char == '/') REMOVE_LAST_CHAR()
-
-    // return new str
-    normalized
 }
