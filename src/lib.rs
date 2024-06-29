8b1a34f30476d39ba5b54f9bfb3e3dacdd2a6fcd
Yuri <49888082+YuriRDev@users.noreply.github.com>
2024-06-29 10:10:58 -0300

    valid redirect

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
-use regex::Regex;
 mod graph;
+mod urldata;
+use graph::Graph;
+use regex::Regex;
+use reqwest::Url;
+use urldata::normalize_url;
 
 #[derive(Debug)]
 struct UrlData {
@@ -7,28 +11,75 @@ struct UrlData {
     url: String,
 }
 
+#[derive(Debug)]
+pub struct Analiser {
+    graph: Graph,
+    queue: Vec<UrlQueue>,
+    max_depth: usize,
+}
 
+#[derive(Debug)]
+struct UrlQueue {
+    depth: usize,
+    url: String
+}
 
+impl Analiser {
+    pub fn new(url: &str, max_depth: usize) -> Analiser {
+        Analiser {
+            graph: Graph::new(),
+            queue: vec![UrlQueue{
+                depth: 1,
+                url: url.to_string()
+            }],
+            max_depth,
+        }
+    }
 
+    fn add_to_queue(&mut self, url: String, depth: usize) {
+        self.queue.push(UrlQueue {
+            depth,
+            url 
+        })
+    }
 
-pub async fn analise_page(url: &str) {
+    pub async fn start(&mut self) {
+        let mut depth = 1;
+        for url in &self.queue {
+            let parent = url.url.to_string();
+            match analise_page(&url.url).await {
+                Some(href_url) => {
+                    for new_url in href_url {
+                        println!("{}", new_url);
+                    }
+                },
+                None => {
+                    println!("Site invalido")
+                } 
+            } 
+        }
+    }
+}
+
+pub async fn analise_page(url: &str) -> Option<Vec<String>> {
+    println!("called! {}", url);
     let reqResult = reqwest::get(url).await;
     match reqResult {
         Err(e) => {
             panic!("[ERROR] Could not find website, check if the URL is correct");
+            return None;
         }
         Ok(content) => match content.text().await {
             Err(e) => {
                 panic!("[ERROR] Failed to read html content");
+                return None;
             }
-            Ok(html) => {
-                extract_strings_from_html(&html, true, "loyola.g12.br");
-            }
+            Ok(html) => return Some(extract_strings_from_html(&html, false, "cpavalanche.net")),
         },
     }
 }
 
-pub fn extract_strings_from_html(text: &str, same_domain: bool, domain: &str) {
+pub fn extract_strings_from_html(text: &str, same_domain: bool, domain: &str) -> Vec<String> {
     let re = Regex::new(r#"[\"'`](.*?)[\"'`]"#).unwrap();
     let mut substrings: Vec<String> = Vec::new();
 
@@ -40,19 +91,22 @@ pub fn extract_strings_from_html(text: &str, same_domain: bool, domain: &str) {
                 || cap[1].ends_with(".jpeg")
                 || cap[1].ends_with(".webp")
                 || cap[1].ends_with(".svg")
+                || cap[1].ends_with(".css")
             {
                 continue;
             }
 
             if same_domain {
                 if is_same_domain(&cap[1], domain) {
-                    println!("{}", &cap[1]);
+                    substrings.push(cap[1].to_string());
                 }
             } else {
-                println!("{}", &cap[1]);
+                substrings.push(cap[1].to_string());
             }
         }
     }
+
+    substrings
 }
 
 fn is_valid_url(url: &str) -> bool {
